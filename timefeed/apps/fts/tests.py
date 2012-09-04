0ee54d6828092e25b750610db531a0552ae68dd4
Nigel Gott <nigel.gott@gmail.com>
2012-09-04 16:27:43 +0100

    Upgraded admin test to check for login, added test admin fixture.

--- timefeed/apps/fts/tests.py
+++ timefeed/apps/fts/tests.py
@@ -1,7 +1,12 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 class AdminSiteTest(LiveServerTestCase):
+    fixtures = ['admin_user.json']
+
+    TEST_ADMIN_USER = 'admin'
+    TEST_ADMIN_PASS = 'ranboll'
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -10,11 +15,29 @@ class AdminSiteTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_access_admin_site_home(self):
+    def test_can_access_admin_and_login(self):
+        login_page_title            = 'Django administration'
+        login_success_page_title    = 'Site administration'
+
+        # Open the browser and check we are on the login screen.
         self.browser.get(self.live_server_url + '/admin/')
 
         body = self.browser.find_element_by_tag_name('body')
-        self.assertIn('Django administration', body.text)
+        self.assertIn(login_page_title, body.text)
 
+        # Enter the password and hit return
+        username_field = self.browser.find_element_by_name('username')
+        username_field.send_keys(AdminSiteTest.TEST_ADMIN_USER)
+
+        password_field = self.browser.find_element_by_name('password')
+        password_field.send_keys(AdminSiteTest.TEST_ADMIN_PASS)
+        password_field.send_keys(Keys.RETURN)
+
+        # Ensure that we successfully logged in
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn(login_success_page_title, body.text)
+        
+    
+        
     
 
